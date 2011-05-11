39a5a4bc1a390ff4dea77c9437330db6d1cf0249
Steven Wilkin <stevenwilkin@gmail.com>
2011-05-11 16:14:45 +0100

    store value from datastore in memcache

--- main.py
+++ main.py
@@ -2,20 +2,31 @@
 
 from google.appengine.ext import webapp, db
 from google.appengine.ext.webapp import util, template
+from google.appengine.api import memcache
 import os
 
 import models
 
 
-class MainHandler(webapp.RequestHandler):
-	def get(self):
-		# retrieve model data
+def get_rpi():
+	# return value from memcache if present
+	data = memcache.get('rpi');
+	if data is not None:
+		return data
+	else:
+		# retrieve from datastore
 		key = db.Key.from_path('Rpi', '1')
 		rpi = db.get(key)
+        memcache.add('rpi', rpi.value)
+        return rpi.value
 
+
+class MainHandler(webapp.RequestHandler):
+	def get(self):
+		rpi = get_rpi()
 		# render view
 		path = os.path.join(os.path.dirname(__file__), 'views', 'index.html')
-		self.response.out.write(template.render(path, {'rpi': rpi.value}))
+		self.response.out.write(template.render(path, {'rpi': rpi}))
 
 
 def main():
