5c695d8fbe28c07615224f8eeaef86a9c73cd286
Steven Wilkin <stevenwilkin@gmail.com>
2011-05-11 14:32:59 +0100

    convert spaces to tabs

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import os
 
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-         path = os.path.join(os.path.dirname(__file__), 'views', 'index.html')
-         self.response.out.write(template.render(path, {'rpi': '5%'}))
+	def get(self):
+		path = os.path.join(os.path.dirname(__file__), 'views', 'index.html')
+		self.response.out.write(template.render(path, {'rpi': '5%'}))
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+	application = webapp.WSGIApplication([('/', MainHandler)],
+										debug=True)
+	util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+	main()
